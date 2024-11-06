b48604ad640b377cdff7817a21436c1097044e0c
rafael-f-dev <rafael.freire.developer@gmail.com>
2024-11-06 10:54:52 +0000

    initial commit

---
+++ pathfinder-client/app/index.js
@@ -0,0 +1,151 @@
+import React, { useState } from 'react';
+import { View, TouchableOpacity, FlatList, Keyboard, StyleSheet} from "react-native";
+import { MD3DarkTheme as DefaultTheme, Searchbar, Text, Button, PaperProvider, ActivityIndicator, Divider } from 'react-native-paper';
+import axios from 'axios';
+
+const Index = () => {
+
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      "primary": "rgb(130, 219, 126)",
+      "onPrimary": "rgb(0, 57, 10)",
+      "primaryContainer": "rgb(0, 83, 18)",
+      "onPrimaryContainer": "rgb(157, 248, 152)",
+      "secondary": "rgb(186, 204, 179)",
+      "onSecondary": "rgb(37, 52, 35)",
+      "secondaryContainer": "rgb(59, 75, 56)",
+      "onSecondaryContainer": "rgb(213, 232, 206)",
+      "tertiary": "rgb(160, 207, 212)",
+      "onTertiary": "rgb(0, 54, 59)",
+      "tertiaryContainer": "rgb(31, 77, 82)",
+      "onTertiaryContainer": "rgb(188, 235, 240)",
+      "error": "rgb(255, 180, 171)",
+      "onError": "rgb(105, 0, 5)",
+      "errorContainer": "rgb(147, 0, 10)",
+      "onErrorContainer": "rgb(255, 180, 171)",
+      "background": "rgb(26, 28, 25)",
+      "onBackground": "rgb(226, 227, 221)",
+      "surface": "rgb(26, 28, 25)",
+      "onSurface": "rgb(226, 227, 221)",
+      "surfaceVariant": "rgb(66, 73, 64)",
+      "onSurfaceVariant": "rgb(194, 201, 189)",
+      "outline": "rgb(140, 147, 136)",
+      "outlineVariant": "rgb(66, 73, 64)",
+      "shadow": "rgb(0, 0, 0)",
+      "scrim": "rgb(0, 0, 0)",
+      "inverseSurface": "rgb(226, 227, 221)",
+      "inverseOnSurface": "rgb(47, 49, 45)",
+      "inversePrimary": "rgb(16, 109, 32)",
+      "elevation": {
+        "level0": "transparent",
+        "level1": "rgb(31, 38, 30)",
+        "level2": "rgb(34, 43, 33)",
+        "level3": "rgb(37, 49, 36)",
+        "level4": "rgb(39, 51, 37)",
+        "level5": "rgb(41, 55, 39)"
+      },
+      "surfaceDisabled": "rgba(226, 227, 221, 0.12)",
+      "onSurfaceDisabled": "rgba(226, 227, 221, 0.38)",
+      "backdrop": "rgba(44, 50, 42, 0.4)"
+    }
+}
+
+  const [query, setQuery] = useState('');
+  const [cities, setCities] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  const fetchCities = async () => {
+    if (!query.trim()) {
+      return; 
+    }
+    setLoading(true);
+
+    try {
+      const response = await axios.post('http://192.168.1.76:4040/search-city', { query });
+      const validCities = response.data.results;
+      setCities(validCities);
+    } catch (error) {
+      console.error('Error fetching city data:', error);
+      setCities([]);
+    }
+    setLoading(false);
+  };
+
+  const handleChangeText = (text) => {
+    setQuery(text);
+  };
+
+  const handleCitySelect = (city) => {
+    setQuery(city.properties.formatted); 
+    setSelectedCity(city); 
+    setCities([]); 
+    Keyboard.dismiss();
+  };
+
+
+  return (
+    <PaperProvider theme={theme}>
+    <View style={[styles.container, {backgroundColor: theme.colors.background}]}>
+
+      <Text style={[styles.title, {color: theme.colors.secondary}]}>Where to next?</Text>
+
+      <Searchbar
+        style={[styles.searchbar, {color: theme.colors.secondary}]}
+        placeholder="Enter a city,  e.g: 'Barcelona, Spain' "
+        placeholderTextColor='#4c5b48'
+        value={query}
+        onChangeText={handleChangeText}
+        />
+
+        <Button style={styles.button} mode='contained' onPress={fetchCities}>Search</Button>
+      
+        {cities.length > 0 && !loading && (
+        <FlatList
+          style={[styles.flatlist, {color: theme.colors.onSecondary}]}
+          data={cities}
+          keyExtractor={(item) => item.properties.place_id.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => handleCitySelect(item)}>
+              <Divider/>
+              <Text style={styles.flatlist_text}>{item.properties.formatted}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      )}
+
+      {loading && <ActivityIndicator animating={true} size="large" color="#0000ff"/>}
+
+    </View>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  searchbar: {
+    margin: 10,
+  },
+  button: {
+    margin: 10,
+  },
+  flatlist: {
+    margin: 10,
+  },
+  flatlist_text: {
+    fontSize: 15,
+    marginVertical: 10,
+  }
+});
+
+export default Index;
---
+++ pathfinder-server/index.js
@@ -0,0 +1,75 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT;
+const openAiKey = process.env.OPENAI_API_KEY;
+const geoapifyKey = process.env.GEOAPIFY_API_KEY;
+
+app.use(cors());
+app.use(express.json());
+
+/////////////////////////////////////////////////////////////////////////
+
+app.post('/generate', async (req, res) => {
+  const { prompt } = req.body;
+
+  if (!prompt) {
+    return res.status(400).json({ error: 'Prompt is required.' });
+  }
+
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+      model: 'gpt-4-mini',
+      messages: [{ role: 'user', content: prompt }],
+      max_tokens: 100,
+      temperature: 0.7,
+    }, {
+      headers: {
+        'Authorization': `Bearer ${openAiKey}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    res.json({ text: response.data.choices[0].message.content });
+  } catch (error) {
+    console.error('Error calling API:', error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+app.post('/search-city', async (req, res) => {
+  const { query } = req.body;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Query is required.' });
+  }
+
+  try {
+    const response = await axios.get('https://api.geoapify.com/v1/geocode/autocomplete', {
+      params: {
+        text: query,
+        apiKey: geoapifyKey,
+        limit: 5,
+      },
+    });
+  
+    const validCities = response.data.features.filter(feature =>
+      feature.properties.city
+    );
+
+    res.json({ results: validCities });
+  } catch (error) {
+    console.error('Error fetching city data:', error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+
+/////////////////////////////////////////////////////////////////////////
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${port}`);
+  });
